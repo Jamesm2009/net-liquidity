e87e399f2ab0938d3617b875a921ed035f835902
Jamesm2009 <ZAFjmcc@yahoo.com>
2026-05-24 20:07:57 -0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import LiquidityDashboard from '@/components/LiquidityDashboard';
+
+export default function Home() {
+  return (
+    <main>
+      <LiquidityDashboard />
+    </main>
+  );
+}
---
+++ lib/redis.ts
@@ -0,0 +1,9 @@
+import { Redis } from '@upstash/redis';
+
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
+
+export const DATA_KEY = 'nlq:data';
+export const UPDATED_KEY = 'nlq:last_updated';
